e710a83cc325c2a263470b0a0870a64b26f3b7ea
aliceinnets <aliceinnets@gmail.com>
2017-01-17 23:52:40 +0100

    added generateUrl to YahooFinance class

--- src/aliceinnets/finance/api/YahooFinance.java
+++ src/aliceinnets/finance/api/YahooFinance.java
@@ -1,5 +1,16 @@
 package aliceinnets.finance.api;
 
 public class YahooFinance {
+	
+	public final static String BASE_URL = "http://finance.yahoo.com/d/quotes.csv";
+	
+	public static String generateUrl(String[] symbols, String[] dataSymbols) {
+		String symbol = symbols[0];
+		for(int i=1;i<symbols.length;++i) symbol += ","+symbols[i];
+		String dataSymbol = dataSymbols[0];
+		for(int i=1;i<dataSymbols.length;++i) dataSymbol += dataSymbols[i];
+		
+		return BASE_URL+"?s="+symbol+"&f="+dataSymbol;
+	}
 
 }
