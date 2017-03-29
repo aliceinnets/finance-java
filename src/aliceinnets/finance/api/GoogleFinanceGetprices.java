1494f358e022fb77257cc36cec8b7ae105ff167e
aliceinnets <aliceinnets@gmail.com>
2017-03-29 13:16:10 +0100

    changed author comment

--- src/aliceinnets/finance/api/GoogleFinanceGetprices.java
+++ src/aliceinnets/finance/api/GoogleFinanceGetprices.java
@@ -10,7 +10,7 @@ import org.jsoup.Jsoup;
  * <p>FIXME beginning and end of summer time introduce extra time zone offset config, which break the system
  * e.g. TIME_ZONE_OFFSET = -240 (from -300) inbetween data</p>
  * 
- * @author alice&lt;aliceinnets@gmail.com&gt;
+ * @author alice &lt;aliceinnets[at]gmail.com&gt;
  *
  */
 public class GoogleFinanceGetprices {
